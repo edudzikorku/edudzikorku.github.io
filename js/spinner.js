cdedf5ecee099a0db026c828d0c798d2793b3daa
edudzikorku <kedudzi007@gmail.com>
2023-04-20 07:09:49 +0000

    info updated

---
+++ js/spinner.js
@@ -0,0 +1,22 @@
+$(document).ready(function () {
+    // $(window).on("load", function() {
+    //     $(".spinner-container").fadeOut(300, function() {
+    //         $("main").css("opacity", 1);
+    //     });
+    // });
+
+    // $(window).on("load", function() {
+    //     $(".spinner-container").fadeOut(3000);
+    // });
+
+    // $("body").addClass("loaded");
+
+
+    $(window).on("load", function() {
+        $(".spinner-container").fadeOut(3000);
+    });
+
+    // $(document).ready(function() {
+    //     $(".spinner-container").fadeOut();
+    // });
+})
--- js/welcome.js
+++ js/welcome.js
@@ -1,27 +1,4 @@
 $(document).ready(function () {
-
-    // $(window).on("load", function() {
-    //     $(".spinner-container").fadeOut(300, function() {
-    //         $("main").css("opacity", 1);
-    //     });
-    // });
-
-    // $(window).on("load", function() {
-    //     $(".spinner-container").fadeOut(3000);
-    // });
-
-    // $("body").addClass("loaded");
-
-
-    $(window).on("load", function() {
-        $(".spinner-container").fadeOut(3000);
-    });
-
-    // $(document).ready(function() {
-    //     $(".spinner-container").fadeOut();
-    // });
-
-
     // Define an array of welcome messages in different languages
     var msgs = [
         "Welcome!",
