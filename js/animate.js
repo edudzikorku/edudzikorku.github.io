059dca8cc37e5306bc8739636729af00d9383f7b
edudzikorku <kedudzi007@gmail.com>
2023-07-08 15:33:26 +0000

    content updated

--- js/animate.js
+++ js/animate.js
@@ -17,6 +17,23 @@ $(document).ready(function () {
         }
     }
 
+    $(window).on("scroll", display);
+
+    function display() {
+        var shows = $(".transition");
+        for(var i = 0; i < shows.length; i++) {
+            var windowHeight = window.innerHeight;
+            var displayTop = shows[i].getBoundingClientRect().top;
+            var displayPoint = 150;
+
+            if(displayTop < windowHeight - displayPoint) {
+                $(shows[i]).addClass('active');
+            } 
+            else {
+                $(shows[i]).removeClass('active');
+            }
+        }
+    }
     var paragraphs = $('.trim');
     var text = paragraphs.text();
     var trimmedText = text.replace(/\s+/g, " ").trim();    
